3efb96172b26785a57453aa024239d1af41d36eb
Dennis Yau <>
2024-06-25 23:49:29 -0700

    connection to atlas

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 const router = require("./src/route/routes");
+const { MongoClient } = require("mongodb");
+dotenv.config();
 
-const port = 3000;
-const uri = "mongodb://localhost:27017/Profile";
+const port = process.env.PORT;
+const uri = process.env.URI2;
 
 //usefule middleware
 app.use(express.json());
@@ -14,7 +16,21 @@ app.use(cors());
 app.use("/api", router);
 
 // The current database to use.
+//new way to connect mongodb
+// const client = new MongoClient(process.env.URI2);
 
+// async function run() {
+//   const connect = client.connect();
+
+//   app.listen(port, () => {
+//     console.log(`connect to port ${port}.`);
+//   });
+//   const db = client.db("Profile");
+//   const collection = db.collection("Profile");
+// }
+// run();
+
+//old version mongodb conneciton
 mongoose
   .connect(uri)
   .then(() => {

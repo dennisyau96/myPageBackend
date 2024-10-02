cdd8a8c8b16498c921397eb94e257cefe2a1274b
Dennis Yau <>
2024-10-02 16:42:08 -0700

    fix

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const uri = process.env.URI2;
 
 //usefule middleware
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
     credentials: true,
--- src/model/skill.model.js
+++ src/model/skill.model.js
@@ -3,6 +3,7 @@ const schema = mongoose.Schema;
 const skillSchema = new schema({
   title: { type: String },
   since: { type: String },
+  remark: { type: Array },
 });
 
 const Skill = mongoose.model("Skill", skillSchema);
